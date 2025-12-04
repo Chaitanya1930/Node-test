e31d4b8378deb4ef2767f29bd1e60c04eb9595e7
Developer <dev@example.com>
2025-12-04 09:20:05 +0000

    Testing CI/CD workflow

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("🚀 Node.js Docker App running on EC2!");
+  res.send("This line is to test CI/CD workflow!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
