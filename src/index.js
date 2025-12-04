a8b4399e1a3dd8b206ad55a2a7fc9b394afbb61c
Developer <dev@example.com>
2025-12-04 09:38:33 +0000

    Updated the code to test cicd

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node.js Docker App running on EC2!");
-  res.send("This line is to test CI/CD workflow!");
+  res.send(`
+    <h1>🚀 Node.js Docker App running on EC2!</h1>
+    <p>This line is to test CI/CD workflow! 👌</p>
+  `);
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
